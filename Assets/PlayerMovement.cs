42d2bd083245b97aaca6a5897cc3a4dded28e523
Mauro Frischherz <mauro.frischherz@gmail.com>
2017-02-21 21:11:02 +0100

    basic rope behaviour (1 link)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,48 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-	public int maxJumps = 2;
 	public float jumpForce = 1;
 	public float aimDistance = 1;
 	public float aimSpeed = 10;
 
 	Rigidbody2D rb;
-	int jumps;
 
 	GameObject target;
 	Vector2 aimDirection;
 	Vector3 aimPosition;
 	Vector2 aimTemp;
 
-	bool castRay = false;
+	DistanceJoint2D hinge;
 
+	bool ropeActive = false;
+
+	LineRenderer lineRenderer;
+	Vector3[] linePoints;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D> ();
-		jumps = 0;
 		target = transform.GetChild (1).gameObject;
+
+		lineRenderer = GetComponentInChildren<LineRenderer> ();
+
+		linePoints = new Vector3[2];
+		for (int i = 0; i < linePoints.Length; i++) {
+			linePoints [i] = Vector3.one;
+		}
+
 		aimDirection = new Vector2 (1, 1).normalized;
 		aimPosition = new Vector3 (1, 1, 0);
 		target.transform.localPosition = aimPosition;
 		target.transform.SetParent (null);
-	}
-
-	void FixedUpdate ()
-	{
-		if (castRay) {
-
-			RaycastHit2D hit;
-			hit = Physics2D.Raycast (aimPosition, aimDirection);
-
-			if (hit.collider != null) {
-				GameObject.Find ("rayTarget").transform.position = hit.point;
-			}
-
-			castRay = false;
-		}
 	}
 	
 	// Update is called once per frame
@@ -64,23 +57,50 @@ public class PlayerMovement : MonoBehaviour
 			aimDirection.Normalize ();
 		}
 
-		// Jump (testing)
-		if (jumps < maxJumps && Input.GetButtonDown ("Jump")) {
-			rb.AddForce (aimDirection * jumpForce, ForceMode2D.Impulse);
-			jumps++;
-			castRay = true;
+		if (ropeActive && Input.GetButton ("RopeIn")) {
+			hinge.distance -= 0.2f;
 		}
+
+		if (ropeActive && Input.GetButton ("RopeOut")) {
+			hinge.distance += 0.2f;
+		}
+
 
 		// update aim target
 		aimPosition.Set (transform.position.x + (aimDirection.x * aimDistance),
 			transform.position.y + (aimDirection.y * aimDistance), 0);
 		target.transform.position = aimPosition;
+
+		linePoints [0] = transform.position;
+
+		lineRenderer.SetPositions (linePoints);
+
+		// Jump (testing)
+		if (Input.GetButtonDown ("Jump")) {
+			if (ropeActive) {
+				ropeActive = false;
+				Destroy (hinge);
+			} else {
+				ropeActive = true;
+				RaycastHit2D hit;
+				hit = Physics2D.Raycast (aimPosition, aimDirection);
+
+				if (hit.collider != null) {
+					GameObject.Find ("rayTarget").transform.position = hit.point;
+					linePoints [1].x = hit.point.x;
+					linePoints [1].y = hit.point.y;
+				}
+					
+				hinge = gameObject.AddComponent (typeof(DistanceJoint2D)) as DistanceJoint2D;
+				hinge.enableCollision = true;
+				hinge.autoConfigureConnectedAnchor = false;
+				hinge.autoConfigureDistance = false;
+				hinge.distance = Vector3.Distance (transform.position, linePoints [1]);
+			}
+		}
+
+		if (ropeActive) {
+			hinge.connectedAnchor = linePoints [1];
+		}
 	}
-
-	void OnCollisionEnter2D (Collision2D collision)
-	{
-		jumps = 0;
-	}
-
-
 }
