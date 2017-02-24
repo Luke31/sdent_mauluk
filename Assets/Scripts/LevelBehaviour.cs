fa46bb3e9cda1bf28949c75a88fa9ca417b83ed0
Mauro Frischherz <mauro.frischherz@gmail.com>
2017-02-24 17:22:42 +0100

    tilemap generated level

---
+++ Assets/Scripts/LevelBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBehaviour : MonoBehaviour
+{
+	public GameObject levelRoot;
+	public Vector3 offset;
+	public float tileSize;
+
+	public Transform[] tilePreFabs;
+
+	public void generate (int[][] tileMap)
+	{
+		for (int x = 0; x < tileMap.Length; x++) {
+			for (int y = 0; y < tileMap [x].Length; y++) {
+				if (tileMap [x] [y] >= 0) {
+					Instantiate (tilePreFabs [tileMap [x] [y]],
+						new Vector3 (x * tileSize + offset.x, -y * tileSize + offset.y, 0),
+						Quaternion.identity);
+				}
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		int[][] tileMap = new int[][] {  
+			new int[]{ 1, 1, 1, 0, 1, 1, 1 },
+			new int[]{ 1, -1, -1, -1, -1, -1, 1 },
+			new int[]{ 1, -1, -1, -1, -1, -1, 1 },
+			new int[]{ 1, -1, -1, -1, -1, -1, 1 },
+			new int[]{ 1, 1, 1, 1, 1, 1, 1 }
+		};
+
+		generate (tileMap);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
