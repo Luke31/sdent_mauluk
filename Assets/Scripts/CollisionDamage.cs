0c3faf8c88db7af7fc2a250bc7f5f9544e4be4b0
Luke31 <lukas.m.schmid@gmail.com>
2017-02-26 17:25:39 +0100

    Add interactive objects (destroyable, bouncyball) -add destroyable box with health, velocity of impacting player is subtracted -add bouncy ball

---
+++ Assets/Scripts/CollisionDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamage : MonoBehaviour
+{
+    [Header("Health")]
+    [Tooltip("Number of hits needed to destroy this object")]
+    public double Health = 100;
+
+    [Header("Trigger damage tag filter")]
+    [Tooltip("Only objects with this tag trigger damage")]
+    public string TagNameFilter = "Player";
+
+    private ParticleSystem _psystem;
+
+    void Start ()
+	{
+	}
+	
+	void Update () {
+        if (Health <= 0) { 
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == TagNameFilter) { 
+            Health -= coll.relativeVelocity.magnitude; //TODO: Include mass for impact
+        }
+    }
+}
