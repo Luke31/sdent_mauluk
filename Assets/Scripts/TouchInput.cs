ec15cf9620a722d3685874f3a3e87a6811fb0376
Luke31 <lukas.m.schmid@gmail.com>
2017-03-01 22:09:48 +0100

    Touchinput for mobile devices

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+	private Dictionary<int, TouchState> touch = new Dictionary<int, TouchState>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Touch a = Input.GetTouch(0);
+
+	}
+}
+
+abstract class TouchState
+{
+	public abstract void Update();
+}
+
+class TouchLeft : TouchState
+{
+	public override void Update()
+	{
+		
+		throw new NotImplementedException();
+	}
+}
+
+class TouchRight : TouchState
+{
+	public override void Update()
+	{
+		throw new NotImplementedException();
+	}
+}
+
+
