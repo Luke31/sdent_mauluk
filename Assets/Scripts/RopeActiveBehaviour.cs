97f042af71ea95789100db874333229b8762686a
Luke31 <lukas.m.schmid@gmail.com>
2017-03-19 19:08:00 +0100

    Initial attempt to use Unity state machine/animation

---
+++ Assets/Scripts/RopeActiveBehaviour.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeActiveBehaviour : PlayerBehaviour {
+
+	public float ropeWidth = 0.2f;
+	public float ropeSplitEpsilon = 0.2f;
+	public float ropeMinLength = 1f;
+	public float ropeFeedSpeed = 0.5f;
+	public float ropeSwingForce = 0.6f;
+
+	Rigidbody2D rb;
+
+	CircleCollider2D playerCollider;
+
+	PhysicsMaterial2D matBouncy;
+	PhysicsMaterial2D matStatic;
+
+	DistanceJoint2D hinge;
+
+
+	int layerMask;
+
+	LineRenderer lineRenderer;
+	Vector3[] linePoints;
+	Vector2 ropeDir;
+
+
+	void resetRope()
+	{
+		linePoints = new Vector3[2];
+		for (int i = 0; i < linePoints.Length; i++)
+		{
+			linePoints[i] = Vector3.zero;
+		}
+
+		lineRenderer.numPositions = linePoints.Length;
+		lineRenderer.SetPositions(linePoints);
+	}
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		Player = GameObject.Find("Player");
+		Target = GameObject.Find("Target");
+
+		lineRenderer = Player.GetComponentInChildren<LineRenderer>();
+		playerCollider = Player.GetComponentInChildren<CircleCollider2D>();
+
+		matBouncy = (PhysicsMaterial2D)Resources.Load("PlayerBouncy");
+		matStatic = (PhysicsMaterial2D)Resources.Load("PlayerStatic");
+
+		layerMask = LayerMask.GetMask("Player");
+		layerMask = ~layerMask;
+
+		resetRope();
+
+		rb = Player.GetComponent<Rigidbody2D>();
+
+		ActivateRope();
+	}
+
+	private void ActivateRope()
+	{
+		RaycastHit2D hit;
+		var playerPos = Player.transform.position;
+		var aimPosition = Target.transform.position;
+		var aimDirection = (aimPosition - playerPos).normalized;
+		hit = Physics2D.Raycast(aimPosition, aimDirection);
+		if (hit.collider != null)
+		{
+
+			Vector2 hullPoint = hit.collider.bounds.ClosestPoint(hit.point);
+			Vector3 hitPoint = new Vector3(hullPoint.x, hullPoint.y);
+
+			if (Vector2.Distance(hitPoint, playerPos) > ropeMinLength)
+			{
+				linePoints[1].x = hitPoint.x;
+				linePoints[1].y = hitPoint.y;
+				
+				lineRenderer.startWidth = ropeWidth;
+				lineRenderer.endWidth = ropeWidth;
+				playerCollider.sharedMaterial = matBouncy;
+
+				hinge = Player.AddComponent(typeof(DistanceJoint2D)) as DistanceJoint2D;
+				hinge.enableCollision = true;
+				hinge.autoConfigureConnectedAnchor = false;
+				hinge.autoConfigureDistance = false;
+				hinge.connectedAnchor = linePoints[1];
+				hinge.distance = Vector3.Distance(playerPos, linePoints[1]);
+			}
+		}
+	}
+
+
+	private Vector2 GetLeftFoce()
+	{
+		return -Vector3.Cross(GetRopeDir(), Vector3.forward).normalized * ropeSwingForce;
+	}
+
+	private Vector2 GetRightFoce()
+	{
+		return -GetLeftFoce();
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		
+		//Horizontal
+		var inputForceHor = animator.GetFloat("HorizontalForce");
+		if (inputForceHor < 0)
+		{
+			//Left 
+			rb.AddForce(GetLeftFoce() * Math.Abs(inputForceHor));
+		}else if(inputForceHor > 0) { //Right
+			rb.AddForce(GetRightFoce() * Math.Abs(inputForceHor));
+		}
+		animator.SetFloat("HorizontalForce", 0);
+
+		//Vertical Rope
+		var inputForceRope = animator.GetFloat("VerticalForce");
+		if (inputForceRope < 0)
+		{
+			//In
+			RaycastHit2D hitFeed = Physics2D.Raycast(Player.transform.position, ropeDir, ropeFeedSpeed * 3, layerMask);
+			if (hitFeed.collider == null && hinge.distance - ropeFeedSpeed > ropeMinLength)
+			{
+				hinge.distance -= ropeFeedSpeed * Math.Abs(inputForceRope);
+			}
+		}
+		else if (inputForceRope > 0)
+		{ //Out
+			RaycastHit2D hitFeed = Physics2D.Raycast(Player.transform.position, -ropeDir, ropeFeedSpeed * 3, layerMask);
+			if (hitFeed.collider == null)
+			{
+				hinge.distance += ropeFeedSpeed * Math.Abs(inputForceRope);
+			}
+		}
+		animator.SetFloat("VerticalForce", 0);
+
+		
+		FixedUpdate();
+		Update();
+	}
+
+	private void Update()
+	{
+		linePoints[0] = Player.transform.position;
+
+		Vector3[] invertedLinePoints = new Vector3[linePoints.Length];
+		for (int i = 0; i < linePoints.Length; i++)
+		{
+			invertedLinePoints[i] = linePoints[linePoints.Length - 1 - i];
+		}
+		lineRenderer.numPositions = invertedLinePoints.Length;
+		lineRenderer.SetPositions(invertedLinePoints);
+	}
+
+	private void FixedUpdate()
+	{
+		linePoints[0] = Player.transform.position;
+		ropeDir = linePoints[1] - linePoints[0];
+
+		RaycastHit2D hitSplit;
+		RaycastHit2D hitLast;
+
+		hitSplit = Physics2D.Raycast(Player.transform.position, ropeDir, ropeDir.magnitude - 0.1f, layerMask);
+
+		if (hitSplit.collider != null)
+		{
+			Vector2 hullPoint = GetClosestPointOnBoundHull(hitSplit.collider, hitSplit.point);
+			Vector3 hitPoint = new Vector3(hullPoint.x, hullPoint.y);
+			float newRopeLength = Vector2.Distance(linePoints[0], hitPoint);
+			float newSplitLength = Vector2.Distance(linePoints[1], hitPoint);
+
+			// A new point in between (far away, enough to warrant a split in the rope)
+			if (newRopeLength > ropeMinLength && newSplitLength > ropeSplitEpsilon)
+			{
+				Vector3[] newPoints = new Vector3[linePoints.Length + 1];
+
+				newPoints[0] = linePoints[0]; // player
+				newPoints[1] = hitPoint;        // new anchor
+				newPoints[2] = linePoints[1];   // last anchor
+
+				for (int i = 2; i < linePoints.Length; i++)
+				{
+					newPoints[i + 1] = linePoints[i];
+				}
+
+				linePoints = newPoints;
+
+				hinge.connectedAnchor = linePoints[1];
+				hinge.distance = Vector3.Distance(Player.transform.position, linePoints[1]);
+			}
+		}
+		else if (linePoints.Length > 2)
+		{
+			Vector2 lastDir = linePoints[2] - linePoints[0];
+			hitLast = Physics2D.Raycast(Player.transform.position, lastDir, lastDir.magnitude - 0.1f, layerMask);
+
+			Vector2 ab = linePoints[1] - linePoints[0];
+			Vector2 bc = linePoints[2] - linePoints[1];
+			Vector2 ac = linePoints[2] - linePoints[0];
+
+			// We see both last points, maybe release one
+			if (hitLast.collider == null && Mathf.Abs(ab.magnitude + bc.magnitude - ac.magnitude) < ropeSplitEpsilon)
+			{
+				Vector3[] newPoints = new Vector3[linePoints.Length - 1];
+
+				newPoints[0] = linePoints[0]; // player
+				newPoints[1] = linePoints[2];   // new anchor = lastAnchor
+
+				for (int i = 2; i < linePoints.Length - 1; i++)
+				{
+					newPoints[i] = linePoints[i + 1];
+				}
+
+				linePoints = newPoints;
+
+				hinge.connectedAnchor = linePoints[1];
+				hinge.distance = Vector3.Distance(Player.transform.position, linePoints[1]);
+			}
+		}
+	}
+
+	private Vector2 GetRopeDir()
+	{
+		return linePoints[1] - linePoints[0];
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		Destroy(hinge);
+		lineRenderer.startWidth = 0;
+		lineRenderer.endWidth = 0;
+		playerCollider.sharedMaterial = matStatic;
+		resetRope();
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	private Vector2 GetClosestPointOnBoundHull(Collider2D collider, Vector2 hitPoint)
+	{
+		Vector2 result = new Vector2();
+		Vector2 tmp;
+
+		Vector2 center = collider.bounds.center;
+		Vector2 extent = collider.bounds.extents;
+
+		float minDist = float.MaxValue;
+		float dist;
+
+		tmp = center + extent;
+		dist = Vector2.Distance(hitPoint, tmp);
+		if (dist < minDist)
+		{
+			minDist = dist;
+			result = tmp;
+		}
+
+		extent.Set(extent.x, -extent.y);
+
+		tmp = center + extent;
+		dist = Vector2.Distance(hitPoint, tmp);
+		if (dist < minDist)
+		{
+			minDist = dist;
+			result = tmp;
+		}
+
+		extent.Set(-extent.x, extent.y);
+
+		tmp = center + extent;
+		dist = Vector2.Distance(hitPoint, tmp);
+		if (dist < minDist)
+		{
+			minDist = dist;
+			result = tmp;
+		}
+
+		extent.Set(extent.x, -extent.y);
+
+		tmp = center + extent;
+		dist = Vector2.Distance(hitPoint, tmp);
+		if (dist < minDist)
+		{
+			minDist = dist;
+			result = tmp;
+		}
+
+		return result;
+	}
+}
