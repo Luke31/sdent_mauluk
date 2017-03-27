8452fce580f0daf9183e81d30f777d0454f58a7f
Mauro Frischherz <mauro.frischherz@gmail.com>
2017-03-27 09:11:58 +0200

    Layer import with properties for Rigidbodies

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -17,10 +17,10 @@ public class LevelBuilder : MonoBehaviour
 	public Vector3 offset;
 	public float tileSize;
 
-	public GameObject collisionModelRoot;
-
 	private Sprite[] tileSprites;
-	private readonly Property worldCollisionComp = new Property { Name = "ComputeCollision" };
+	private readonly Property computeCollisionComp = new Property { Name = "ComputeCollision" };
+	private readonly Property rigidBodyComp = new Property { Name = "Rigidbody" };
+	private readonly Property rigidBodyMassComp = new Property { Name = "RigidbodyMass" };
 	private readonly Property depthPropComp = new Property { Name = "Depth" };
 
 
@@ -28,7 +28,21 @@ public class LevelBuilder : MonoBehaviour
 	public void Generate (TiledMap map)
 	{
 		foreach (TiledLayer layer in map.Layer) {
+			// Check layer properties
+			Property computeCollisionProp = layer.Properties.PropertyList.Find (x => x.Equals (computeCollisionComp));
+			bool computeCollision = computeCollisionProp != null;
+
+			Property rigidbodyProp = layer.Properties.PropertyList.Find (x => x.Equals (rigidBodyComp));
+			bool isRigidbody = rigidbodyProp != null;
+
 			int[,] dataMap = layer.DataMap;
+			bool[,] objectMap = new bool[layer.Width, layer.Height];
+
+			GameObject currentLayer = new GameObject (layer.Name);
+			currentLayer.transform.SetParent (levelRoot.transform);
+			GameObject currentObject = null;
+			BoxCollider2D currentCollider = null;
+
 
 			float depth = 0;
 
@@ -37,89 +51,88 @@ public class LevelBuilder : MonoBehaviour
 				depth = float.Parse (depthProp.Value);
 			}
 
-			for (int x = 0; x < layer.Width; x++) {
-				for (int y = 0; y < layer.Height; y++) {
-					if (dataMap[x,y] >= 0) {
-						GameObject instance = Instantiate (baseTilePrefab,
-							                      new Vector3 ((x + layer.Offsetx) * tileSize + offset.x,
-								                      -(y + layer.Offsety) * tileSize + offset.y, depth),
-							                      Quaternion.identity);
+			for (int y = 0; y < layer.Height; y++) {
+				for (int x = 0; x < layer.Width; x++) {
+					if (dataMap [x, y] >= 0 && !objectMap [x, y]) {
+						// Create parent GameObject
+						currentObject = new GameObject(x + "_" + y);
+						currentObject.transform.SetParent (currentLayer.transform);
 
-						instance.transform.SetParent (transform);
-						instance.transform.localScale *= tileSize;
-
-						instance.GetComponentInChildren<SpriteRenderer> ().sprite = tileSprites [dataValue];
-					}
-				}
-			}
-
-
-			Property worldCollision = layer.Properties.PropertyList.Find (x => x.Equals (worldCollisionComp));
-			if (worldCollision != null && worldCollision.Value == "true") {
-				WorldCollision (layer, dataMap);
-			}
-
-		}
-	}
-
-	// Computes bounding boxes around free floating objects
-	private void ObjectCollision(TiledLayer layer, int[,] dataMap) {
-		
-	}
-
-	// Computes bounding boxes that are as big as possible
-	private void WorldCollision (TiledLayer layer, int[,] dataMap)
-	{
-		bool[,] colliderMap = new bool[layer.Width, layer.Height];
-
-		BoxCollider2D box = null;
-		// Create collision model
-		for (int y = 0; y < layer.Height; y++) {
-			for (int x = 0; x < layer.Width; x++) {
-				if (dataMap [x, y] >= 0 && !colliderMap [x, y]) {
-					box = collisionModelRoot.AddComponent<BoxCollider2D> ();
-					box.size = new Vector2 (tileSize, tileSize);
-					box.offset = new Vector2 ((x + layer.Offsetx) * tileSize + offset.x,
-						-(y + layer.Offsety) * tileSize + offset.y);
-
-					colliderMap [x, y] = true;
-
-					// grow box horizontally
-					int rectWidth = 1;
-					for (int xx = x + 1; xx < layer.Width; xx++) {
-						if (dataMap [xx, y] >= 0) {
-							box.size += new Vector2 (tileSize, 0);
-							box.offset += new Vector2 (tileSize / 2f, 0);
-							rectWidth++;
-
-							colliderMap [xx, y] = true;
-						} else {
-							break;
+						// Compute collision box
+						if (computeCollision) {
+							currentCollider = currentObject.AddComponent<BoxCollider2D> ();
+							currentCollider.size = new Vector2 (tileSize, tileSize);
+							currentCollider.offset = new Vector2 ((x + layer.Offsetx) * tileSize + offset.x,
+								-(y + layer.Offsety) * tileSize + offset.y);
 						}
-					}
 
-					// grow box vertically
-					for (int yy = y + 1; yy < layer.Height; yy++) {
-						bool sameWidth = true;
-						for (int xx = x; xx < x + rectWidth; xx++) {
-							if (dataMap [xx, yy] < 0) {
-								sameWidth = false;
+						if (isRigidbody) {
+							Rigidbody2D rigidbody = currentObject.AddComponent<Rigidbody2D> ();
+
+							Property rigidbodyMass = layer.Properties.PropertyList.Find (c => c.Equals (rigidBodyMassComp));
+							if (rigidbodyMass != null) {
+								rigidbody.mass = float.Parse(rigidbodyMass.Value);
+							}
+						}
+
+						objectMap [x, y] = true;
+
+						// grow box horizontally
+						int rectWidth = 1;
+						for (int xx = x + 1; xx < layer.Width; xx++) {
+							if (dataMap [xx, y] >= 0) {
+								if (computeCollision) {
+									currentCollider.size += new Vector2 (tileSize, 0);
+									currentCollider.offset += new Vector2 (tileSize / 2f, 0);
+								}
+								rectWidth++;
+
+								objectMap [xx, y] = true;
+							} else {
 								break;
 							}
 						}
-						if (!sameWidth) {
-							break;
-						} else {
-							box.size += new Vector2 (0, tileSize);
-							box.offset -= new Vector2 (0, tileSize / 2f);
 
+						// grow box vertically
+						for (int yy = y + 1; yy < layer.Height; yy++) {
+							bool sameWidth = true;
 							for (int xx = x; xx < x + rectWidth; xx++) {
-								colliderMap [xx, yy] = true;
+								if (dataMap [xx, yy] < 0) {
+									sameWidth = false;
+									break;
+								}
+							}
+							if (!sameWidth) {
+								break;
+							} else {
+								if (computeCollision) {
+									currentCollider.size += new Vector2 (0, tileSize);
+									currentCollider.offset -= new Vector2 (0, tileSize / 2f);
+								}
+
+								for (int xx = x; xx < x + rectWidth; xx++) {
+									objectMap [xx, yy] = true;
+								}
 							}
 						}
 					}
+
+					// Instantiate tile
+					if (dataMap [x, y] >= 0) {
+						// Instantiate tile
+						GameObject instance = Instantiate (baseTilePrefab,
+							new Vector3 ((x + layer.Offsetx) * tileSize + offset.x,
+								-(y + layer.Offsety) * tileSize + offset.y, depth),
+							Quaternion.identity);
+
+						instance.transform.SetParent (currentObject.transform);
+						instance.transform.localScale *= tileSize;
+
+						instance.GetComponentInChildren<SpriteRenderer> ().sprite = tileSprites [dataMap[x,y]];
+					}
 				}
 			}
+
 		}
 	}
 
